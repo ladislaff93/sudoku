f10cd4a8741302cce253862a0343a18931181f35
ladislaff93 <lado93@gmail.com>
2021-06-13 10:37:24 +0200

    13.6

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,1 +1,16 @@
-import pygame as p
+import pygame as pg
+
+SIZE = 750,750
+FPS = 60
+pg.init()
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption('Sudoku') 
+
+def main():
+    run = True 
+    while run:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+
+main()
